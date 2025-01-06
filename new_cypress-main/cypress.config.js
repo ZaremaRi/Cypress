90859eee16f09256ece0f896cb0d9d1902da8317
ZaremaRi <zaremarinatovna23@gmail.com>
2025-01-06 14:03:44 +0700

    Add files via upload

---
+++ new_cypress-main/cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    viewportHeight: 932,
+    viewportWidth: 430,
+    blockHosts: ["*mc.yandex.ru"],
+    baseUrl:"https://login.qa.studio"
+  },
+});
+
+// Все параметры конфига: https://docs.cypress.io/guides/references/configuration
